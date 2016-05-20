14d8948b3efb805424364c703f63a69a2e25bf09
Crazypkr1099 <nicholas.lacasse430@gmail.com>
2016-05-20 00:06:15 -0400

    Working application

--- zipline3/javascript.js
+++ zipline3/javascript.js
@@ -4,7 +4,7 @@
 // viewers -- viewer count
 // status -- Stream name
 
-var streamers = ["Crazypkr1099", "Papatooshi", "Freecodecamp", "Xartuo","CloakedYoshi","Nom_in_HD","LeFrenchStallion","DegenTP","Sinqnew","PangaeaPanga"];
+var streamers = ["Crazypkr1099", "Papatooshi", "Freecodecamp", "Xartuo", "CloakedYoshi", "Nom_in_HD", "LeFrenchStallion", "DegenTP", "Sinqnew", "PangaeaPanga"];
 
 var JSONOnline = function(jsonRead) {
     var logoURL = "http://www-cdn.jtvnw.net/images/xarth/404_user_50x50.png"
@@ -18,17 +18,18 @@ var JSONOnline = function(jsonRead) {
               <div id="streamerLogo"><img src="${logoURL}"></img></div>
               <div id="streamerName">${jsonRead.stream.channel.display_name}</div>
               <div id="streamerStatus" data-status="online">Online</div>
-          </div>
-          <div class="popup" id="active">
+            </div>
+          <div class="popup" id="inactive">
             <div class="popup_content">
               <div id="streamerTitle">${jsonRead.stream.channel.status}</div>
               <div id="streamerViewers">~ Viewers: ${jsonRead.stream.viewers}</div>
               <div id="streamerFollowers">~ Followers: ${jsonRead.stream.channel.followers}</div>
             </div>
             <div class="popup_image"><img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_${(jsonRead.stream.channel.display_name).toLowerCase()}-300x150.jpg"></img></div>
-          </div>
+            </div>
           </section>`)
         $(".container").append(div);
+        createClickers(jsonRead.stream.channel.display_name);
     }
 }
 
@@ -48,21 +49,36 @@ var JSONOffline = function(jsonRead) {
              <div id="streamerLogo"><img src="${logoURL}"></img></div>
              <div id="streamerName">${jsonRead.display_name}</div>
              <div id="streamerStatus" data-status="offline">Offline</div>
-         </div>
-         <div class="popup" id="active">
+            </div>
+         <div class="popup" id="inactive">
            <div class="popup_content">
              <div id="streamerTitle">Currently Not Streaming... </div>
              <div id="streamerViewers">~ Total Views: ${jsonRead.views}</div>
              <div id="streamerFollowers">~ Followers: ${jsonRead.followers}</div>
            </div>
            <div class="popup_image"><img src="${profile_banner}"></img></div>
-         </div>
+           </div>
          </section>`)
         $(".container").append(div);
+
+        createClickers(jsonRead.display_name);
     }
 }
 
-var JSON = function(sort){
+
+var createClickers = function(streamer_name) {
+    $("." + streamer_name).click(function() {
+      var idPath = $(this).children().siblings()[1];
+      if ($(idPath).attr("id") == "inactive"){
+        $(idPath).attr("id","active");
+      }else{
+        $(idPath).attr("id","inactive");
+      }
+    });
+    $("." + streamer_name).animate({width: '100%', marginLeft: "0px"});
+}
+
+var JSON = function(sort) {
     streamers.forEach(function(streamer_name) {
         $.ajax({
             url: `https://api.twitch.tv/kraken/streams/${streamer_name}`,
@@ -90,7 +106,7 @@ var JSON = function(sort){
                             success: function(jsonReadOffline) {
                                 JSONOffline(jsonReadOffline);
                             }
-                        });
+                        })
                     }
                 }
                 if (sort != "online" && jsonReadOnline.stream == null) {
@@ -100,14 +116,42 @@ var JSON = function(sort){
                         success: function(jsonReadOffline) {
                             JSONOffline(jsonReadOffline);
                         }
-                    });
+                    })
                 }
             }
-        });
+        })
     });
 }
 
-
 $(document).ready(function() {
     JSON("friends");
+    $("nav #online").click(function(){
+      $("section").remove();
+      $('nav li').each(function () {
+            $(this).removeClass("selected");
+        })
+        $(this).addClass("selected");
+      JSON("online");
+    });
+    $("nav #offline").click(function(){
+      $("section").remove();
+      $('nav li').each(function () {
+            $(this).removeClass("selected");
+        })
+        $(this).addClass("selected");
+      JSON("offline");
+    });
+    $("nav #friends").click(function(){
+      $("section").remove();
+      $('nav li').each(function () {
+            $(this).removeClass("selected");
+        })
+        $(this).addClass("selected");
+      JSON("friends");
+    });
+
+
+
+
+
 });
